52dc248fae6af7bd1bc68b7d14a168ca18561eaa
Ashot <ashot.todo@gmail.com>
2018-07-11 14:08:18 +0200

    added ejs view engine and layout support

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const layout = require('express-ejs-layouts');
 const express = require('express');
 const config = require('config');
 const router = require('router');
+const path = require('path');
+const ejs = require('ejs');
 
 module.exports.start = () => {
     /* Initiate application instance and router */
@@ -11,6 +14,16 @@ module.exports.start = () => {
 
     /* Removing express header */
     app.disable('x-powered-by');
+
+    /* Configuring view engine */
+    app.engine('.html', ejs.__express);
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, config.views));
+
+    /* Configuring layout */
+    app.use(layout);
+
+    app.use(express.static(path.join(__dirname, config.assets)));
 
     /* Initiate router paths within the app */
     app.use('/', exRouter);
